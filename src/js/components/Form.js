e36100b68ceaa22de0ef1d857e2068f52ef6246f
Denis <dev.denism@yandex.ru>
2020-07-02 10:47:50 +1000

    #5 + build

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -20,7 +20,16 @@ const Form = ( {addPost} ) => {
 
   const handleSubmit = ( ev ) => {
     ev.preventDefault();
-    addPost( makePost( shortid.generate(), {userName, commentBody} ) );
+    addPost(
+      makePost( shortid.generate(),
+                {
+                  userName,
+                  commentBody,
+                  dateTime: new Date().toLocaleString()
+                }
+      )
+    );
+
     setUserName( '' );
     setCommentBody( '' );
   };
