80b11c37d0b1ee1215b7969d75bd861a8e518746
Denis <dev.denism@yandex.ru>
2020-07-03 01:43:14 +1000

    upmerge

--- src/js/redux/Posts/actions.js
+++
@@ -1,8 +0,0 @@
-export const DELETE_POST = 'STORAGE/DELETE_POST';
-export const ADD_POST = 'STORAGE/ADD_POST';
-export const UPDATE_POSTS = 'APP/UPDATE_POSTS';
-
-export const addPost = ( post ) => ( {type: ADD_POST, post} );
-export const deletePost = ( hash ) => ( {type: DELETE_POST, hash} );
-export const updatePosts = ( posts ) => ( {type: UPDATE_POSTS, posts})
-
